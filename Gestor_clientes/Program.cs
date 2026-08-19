f72e4debcea355e0a954200d8e2186b9f56f58e2
David Hdez <David@hernandezperona.com>
2026-08-19 12:24:18 +0200

    Añadir List<Cliente>, alta de clientes de prueba y recorrido con foreach

--- Gestor_clientes/Program.cs
+++ Gestor_clientes/Program.cs
@@ -4,8 +4,20 @@
     {
         private static void Main(string[] args)
         {
-            Cliente cliente1 = new Cliente(1, "David", "Hernández", "600123456", "david@mail.com", DateTime.Now);
-            Console.WriteLine(cliente1);
+            Cliente cliente1 = new(1, "David", "Hernández", "600123456", "david@mail.com", DateTime.Now);
+
+            List<Cliente> clientes = new();
+            clientes.Add(cliente1);
+
+            Cliente cliente2 = new Cliente(2, "Laura", "Gómez", "611987654", "laura@mail.com", DateTime.Now);
+            clientes.Add(cliente2);
+
+            foreach (Cliente c in clientes)
+            {
+                Console.WriteLine(c);
+            }
+
+            Console.WriteLine($"La lista tiene {clientes.Count} cliente(s).");
         }
     }
 }
